b022c925ad049db2c2d3fb254f319a1ef613a506
data Test <phuocvk9000@gmail.com>
2025-04-08 21:54:51 +0700

    Exception use Try catch

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int result;
+        while (true) {
+            System.out.print("Enter your Number: ");
+            try{
+                int number = scanner.nextInt();
+                result = (10/number);
+                System.out.println("kết quả là: "+result);
+                break;
+            }catch (Exception e) {
+                System.out.println(" số cần chia không hợp lệ!! ");
+                System.out.println(" Vui lòng nhập lại ");
+            }finally{
+                System.out.println("hello");
+            }
+        }
+    }
+}
